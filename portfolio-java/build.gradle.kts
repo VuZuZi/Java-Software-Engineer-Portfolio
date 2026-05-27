6be36742e0b9f6892c06048be531b4aeb41251ca
vuzuzi <dinhvuht01@gmail.com>
2026-05-28 00:57:20 +0700

    index on main: eccea8a Merge pull request #1 from VuZuZi/frontend

--- portfolio-java/build.gradle.kts
+++ portfolio-java/build.gradle.kts
@@ -22,10 +22,10 @@ dependencies {
     // ===============================
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+//    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-devtools")
 
@@ -49,6 +49,13 @@ dependencies {
     // ===============================
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    // ===============================
+    // 🔐 JWT (BẮT BUỘC)
+    // ===============================
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     // ===============================
     // 🧪 TEST
